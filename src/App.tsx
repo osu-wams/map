1351b5beddd108df82a51b4856f2cb44c453c7ca
mcdonalm <michael.mcdonald@oregonstate.edu>
2019-11-20 21:28:58 -0800

    JSON Google Map Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import Map from './map/Map';
-
+const lightStyle = require("./map/styles/light_map.json");
+const darkStyle = require("./map/styles/dark_map.json");
 const apiKey = process.env.REACT_APP_GA_MAPS_KEY as string;
 const defaultProps = {
   apiKey,
@@ -9,7 +10,10 @@ const defaultProps = {
     lat: 44.5615578,
     lng: -123.2780367
   },
-  zoom: 16
+  zoom: 16,
+  options: {
+    styles: lightStyle
+  }
 };
 const App = () => (
   <div className="App">
